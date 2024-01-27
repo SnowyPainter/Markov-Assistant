afd66759e9dfc9e1f2dcfdaf4ea485fb9f6cebbf
Minwoo Yu <snowypainter@gmail.com>
2024-01-27 22:16:26 +0900

    fix legacy problems

--- test.py
+++ test.py
@@ -5,11 +5,6 @@ y3_long_nvda_symbols = ["nvda", "^vix", "tqqq", "xsd", "eurusd=x"]
 mo1_5m_long_nvda_tickers = ["nvda", "^vix", "tqqq", "xsd", "eurusd=x"]
 updown_symbols = ["nvda", "amd", "intc"]
 
-def test():
-    df = data.create_dataset(mo1_5m_long_nvda_tickers, start=data.today_before(data.days_to_years(3)), end=data.today(), interval='1d')
-    df = models.lstm_prepare_dataset(df)
-    models.test(df.values)
-
 def get_tomorrow_updown(symbols):
     lags = 10
     train = data.create_dataset(symbols, start=data.today_before(data.days_to_years(3)), end=data.today(), interval='1d')
@@ -20,7 +15,7 @@ def get_tomorrow_updown(symbols):
     
     lstm = models.LSTM_Sequential(lags, len(symbols), 10, 100, verbose=True)
     lstm.compile()
-    lstm.fit(train_values, 100, 10, './models/lstm.h5', skip_if_exist=True)
+    lstm.fit(train_values, 100, 10, './models/lstm.keras', skip_if_exist=True)
     
     return lstm.predict(test_values)
 
@@ -30,10 +25,11 @@ def get_long_term_prediction(symbols):
     X_train, X_test, y_train, y_test = models.dnn_prepare_dataset(df, lags, "nvda_Price")
     dnn = models.DNN(X_train, X_test, y_train, y_test, verbose=2)
     dnn.compile()
-    dnn.fit(100, './models/dnn.h5', skip_if_exist=True)
+    dnn.fit(100, './models/dnn.keras', skip_if_exist=True)
     
+    df = data.create_dataset(symbols, start=data.today_before(data.days_to_years(3)), end=data.today(), interval='1d')
     df = models.dnn_prepare_for_prediction_dataset(df, lags)
-    print(dnn.predict(df))
-    
+    return dnn.predict(df)
+
 #print(get_tomorrow_updown(updown_symbols))
 #print(get_long_term_prediction(y3_long_nvda_symbols))
