4876b93bf16f09aa04925de1b80ec3fbe49ea478
Minwoo Yu <snowypainter@gmail.com>
2024-01-30 03:07:08 +0900

    add error handler

--- handlers/window_handler.py
+++ handlers/window_handler.py
@@ -30,3 +30,24 @@ class Handler:
         elif(infotype == tradeinfo.InfoType.CLOSINGOUT):
             self.previous_net_wealth = net_wealth
             QMessageBox.information(self, 'CLOSED OUT', f'performance : {info.performance}, net : {net_wealth}')
+    
+    def show_error(self, message):
+        QMessageBox.critical(self, "Error", f"{message}")
+    
+    def handle_train_init_error(self, lags, symbols, target=None):
+        lags_limit = 13
+        if lags > lags_limit:
+            self.show_error(f"lags({lags}) value is too big.")
+            return -1
+        if len(symbols) < 2:
+            self.show_error(f"Symbol list size must be more than 2.")
+            return -1
+        if target != None and target == "":
+            self.show_error(f"Target shouldn't be empty.")
+            return -1
+        return 0
+    def handle_flaw_dataset(self, df):
+        if df.empty:
+            self.show_error(f"Dataset made from your symbols is empty. Check your symbols")
+            return -1
+        return 0
